71c5b409c161a4da74b9fb8e4b6a741122064260
hpMaster <student@ubuntu.ubuntu-domain>
2016-03-31 21:13:15 +0200

    login update

---
+++ src/main/java/my/vaadin/app/LoginView.java
@@ -0,0 +1,107 @@
+package my.vaadin.app;
+
+import com.vaadin.data.validator.AbstractValidator;
+import com.vaadin.data.validator.EmailValidator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
+
+public class LoginView extends CustomComponent implements View,
+        Button.ClickListener {
+
+    public static final String NAME = "login";
+
+    private final TextField user;
+
+    private final PasswordField password;
+
+    private final Button loginButton;
+
+    public LoginView() {
+        setSizeFull();
+
+        user = new TextField("User:");
+        user.setWidth("300px");
+        user.setRequired(true);
+        user.setInvalidAllowed(false);
+
+        password = new PasswordField("Password:");
+        password.setWidth("300px");
+        password.setRequired(true);
+        password.addValidator(new PasswordValidator());
+        password.setValue("");
+        password.setNullRepresentation("");
+
+        loginButton = new Button("Login", this);
+
+        VerticalLayout fields = new VerticalLayout(user, password, loginButton);
+        fields.setCaption("Please login to access the application. (test@test.com/passw0rd)");
+        fields.setSpacing(true);
+        fields.setMargin(new MarginInfo(true, true, true, false));
+        fields.setSizeUndefined();
+
+        VerticalLayout viewLayout = new VerticalLayout(fields);
+        viewLayout.setSizeFull();
+        viewLayout.setComponentAlignment(fields, Alignment.MIDDLE_CENTER);
+        viewLayout.setStyleName(Reindeer.LAYOUT_BLUE);
+        setCompositionRoot(viewLayout);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        user.focus();
+    }
+
+    // Validator for validating the passwords
+    private static final class PasswordValidator extends AbstractValidator<String> {
+
+        public PasswordValidator() {
+            super("password invalid");
+        }
+
+        @Override
+        protected boolean isValidValue(String value) {
+            if (value != null && value.length() < 5) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public Class<String> getType() {
+            return String.class;
+        }
+    }
+
+    public void buttonClick(ClickEvent event) {
+        if (!user.isValid() || !password.isValid()) {
+            return;
+        }
+
+        String username = user.getValue();
+        String password = this.password.getValue();
+
+        boolean isValid = username.equals("test")
+                && password.equals("test");
+        if (isValid) {
+
+            getSession().setAttribute("user", username);
+            getUI().getNavigator().navigateTo(LoggedView.NAME);
+
+        } else {
+
+            // Wrong password clear the password field and refocuses it
+            this.password.setValue(null);
+            this.password.focus();
+
+        }
+    }
+}
